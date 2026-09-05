a110785f7c3865718121686261990c6a1afc1c24
BuddhimaDishantha <dishanthabuddhima@gmail.com>
2026-09-05 11:32:19 +0530

    feat: initialize Spring Boot backend

---
+++ backend/src/test/java/com/careerpulse/api/CareerPulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerpulse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
